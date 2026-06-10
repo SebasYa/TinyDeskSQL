ceb0c48dea4d4703df446d1bbf772d97de8ca115
Max <maxibian16@gmail.com>
2026-06-09 21:29:57 -0300

    creacion vista Proyectos Activos

--- VistasProyectos.sql
+++ VistasProyectos.sql
@@ -1,12 +1,12 @@
----------------Vistas Estado---------------------
+---------------Vistas Proyectos---------------------
 
 CREATE VIEW vw_ProyectoEstado AS
 SELECT P.Nombre, P.Descripcion,P.FechaInicio,P.FechaFin,P.Activo, E.Nombre AS Estado
 FROM PROYECTO AS P
 INNER JOIN ESTADO AS E
- ON P.IdEstado = E.Id;
+ON P.IdEstado = E.Id;
 
 
-
-
-
+CREATE VIEW vw_ProyectosActivos AS
+SELECT * FROM vw_ProyectoEstado
+WHERE Activo = 1;
